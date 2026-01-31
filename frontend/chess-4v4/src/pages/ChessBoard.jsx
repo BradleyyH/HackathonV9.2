5bc16f92af33ba00dabef6062a2422c882e5ff47
Travis Kayode <zmac346@live.rhul.ac.uk>
2026-01-31 21:40:24 +0000

    Chess handling

--- frontend/chess-4v4/src/pages/ChessBoard.jsx
+++ frontend/chess-4v4/src/pages/ChessBoard.jsx
@@ -1,10 +1,26 @@
-// src/pages/ChessBoard.jsx
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js';
+import io from 'socket.io-client';
 
 function ChessBoard() {
   const [game, setGame] = useState(new Chess());
+  const [socket, setSocket] = useState(null);
+
+ 
+
+  useEffect(() => {
+    const newSocket = io('http://10.2.189.7:5000/');
+    setSocket(newSocket);
+
+    newSocket.on('move', (move) => {
+      const gameCopy = new Chess(game.fen());
+      gameCopy.move(move);
+      setGame(gameCopy);
+    });
+
+    return () => newSocket.close();
+}, []);
 
   function onDrop(sourceSquare, targetSquare) {
     try {
@@ -18,7 +34,14 @@ function ChessBoard() {
       if (move === null) return false;
       
       setGame(gameCopy);
-      // TODO: WebSocket send to backend
+      
+      if (socket) {
+        socket.emit('moves', {
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: 'q'
+        });
+      }
       return true;
     } catch (error) {
       return false;
